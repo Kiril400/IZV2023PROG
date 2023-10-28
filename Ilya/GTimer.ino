928574046423909bda4185c593a838e2391f6edf
ilyareutin <144676168+ilyareutin@users.noreply.github.com>
2023-10-28 22:41:40 +0300

    Update GTimer.ino

--- Ilya/GTimer.ino
+++ Ilya/GTimer.ino
@@ -1,9 +1,9 @@
 #include "GyverTimer.h"
-GTimer_ms myTimer;
+GTimer_ms myTimer;               // create timer
 
 void setup() {
   Serial.begin(9600);
-  myTimer.setInterval(500);
+  myTimer.setInterval(500);   // set interval
 }
 
 void loop() {
