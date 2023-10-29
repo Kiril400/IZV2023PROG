47fc641e7efb81ed1a405a25ed0367845e813db7
ilyareutin <144676168+ilyareutin@users.noreply.github.com>
2023-10-29 20:46:49 +0200

    Update input and control.ino

--- Ilya/input and control.ino
+++ Ilya/input and control.ino
@@ -1,1 +1,15 @@
+/*
+  TTP223B-Capacitive-Touch-Switch-Module
+*/
+const int SENSOR_PIN = 2;          
+void setup() {
+  Serial.begin(9600);
+  pinMode(SENSOR_PIN, INPUT);
+}
 
+void loop() {
+  if(digitalRead(SENSOR_PIN) == HIGH){
+    Serial.println("Sensor is touched");
+    while(digitalRead(SENSOR_PIN) == HIGH){}
+  }
+}
