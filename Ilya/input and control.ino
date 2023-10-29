82d5888db0c541f3baef3b28f7746008f35787fb
ilyareutin <144676168+ilyareutin@users.noreply.github.com>
2023-10-30 00:23:54 +0200

    Update input and control.ino

--- Ilya/input and control.ino
+++ Ilya/input and control.ino
@@ -1,15 +1,23 @@
 /*
   TTP223B-Capacitive-Touch-Switch-Module
 */
-const int SENSOR_PIN = 2;          
+const int SENSOR_PIN = 2;
+// Declare a constant integer SENSOR_PIN and set it to pin 2. This is the pin connected to the touch sensor.
+
 void setup() {
   Serial.begin(9600);
+  // Initialize serial communication at a baud rate of 9600 for debugging output.
   pinMode(SENSOR_PIN, INPUT);
+  // Set the SENSOR_PIN as an input to read the touch sensor.
 }
 
 void loop() {
-  if(digitalRead(SENSOR_PIN) == HIGH){
+  if (digitalRead(SENSOR_PIN) == HIGH) {
+    // Check if the digital signal read from SENSOR_PIN is HIGH, which indicates the sensor is touched.
     Serial.println("Sensor is touched");
-    while(digitalRead(SENSOR_PIN) == HIGH){}
+    // Print a message indicating that the sensor is touched.
+    while (digitalRead(SENSOR_PIN) == HIGH) {}
+    // Wait in a loop until the sensor is no longer touched (the signal becomes LOW).
   }
 }
+// The main loop of the program continuously checks the status of the touch sensor and prints a message when it's touched.
