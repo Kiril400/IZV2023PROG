b2c3019fa6050134af08e8e2b7e2ce05842522ab
ilyareutin <144676168+ilyareutin@users.noreply.github.com>
2023-10-30 00:12:48 +0200

    Update GTimer.ino

--- Ilya/GTimer.ino
+++ Ilya/GTimer.ino
@@ -1,11 +1,19 @@
 #include "GyverTimer.h"
-GTimer_ms myTimer;               // create timer
+// Include the GyverTimer library for timer functionality.
+
+GTimer_ms myTimer;
+// Create an instance of the GTimer_ms class called "myTimer" for millisecond-based timing.
 
 void setup() {
   Serial.begin(9600);
-  myTimer.setInterval(500);   // set interval
+  // Initialize serial communication at a baud rate of 9600.
+  myTimer.setInterval(500); 
+  // Set the interval for "myTimer" to 500 milliseconds (0.5 seconds).
 }
 
 void loop() {
-  if (myTimer.isReady()) Serial.println("Timer!");
+  if (myTimer.isReady())
+  // Check if the timer "myTimer" has reached its set interval.
+    Serial.println("Timer!");
+  // If the timer is ready, print "Timer!" to the serial monitor.
 }
