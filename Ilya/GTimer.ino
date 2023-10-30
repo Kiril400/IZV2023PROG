06cf11a27496df2b5c7a9c8004ce41f011fdf61b
ilyareutin <144676168+ilyareutin@users.noreply.github.com>
2023-10-30 12:33:06 +0200

    Update GTimer.ino

--- Ilya/GTimer.ino
+++ Ilya/GTimer.ino
@@ -1,3 +1,7 @@
+// The code demonstrates the usage of the "GyverTimer" library, specifically the GTimer_ms class, to create a timer that triggers an action at regular intervals.
+// This code sets up a millisecond-based timer (myTimer) with a 500-millisecond interval. 
+// In the loop function, it continuously checks if the timer has reached its interval and prints "Timer!" to the serial monitor when it does. 
+// This demonstrates how to use a timer to trigger actions at specific time intervals.
 #include "GyverTimer.h"
 // Include the GyverTimer library for timer functionality.
 
